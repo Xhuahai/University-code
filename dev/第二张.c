212bda2c8051cec7a7d57d8c4944a586a6399503
unknown <2019297208@qq.com>
2023-03-11 15:23:27 +0800

    code

---
+++ dev/第二张.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#define PI 3.14
+#include<math.h>
+int main()
+{	//int a = 3;
+//	int b = 4;
+//	int c = 5;
+//	int s = (a + b + c)/2;
+//	float area = sqrt(s*(s-a)*(s-b)*(s-c));
+//	printf("三角形的三边：\n %d %d %d",a,b,c); 
+//	printf("面积是%f",area); 
+	//int a,b,c,s;
+//	printf("请输入梯形的上底：\n");
+//	scanf("%d",&a);
+//	printf("请输入梯形下底：\n");
+//	scanf("%d",&b);
+//	printf("请输入梯形高：\n");
+//	scanf("%d",&c);
+//	//scanf("%d%d%d", &a,&b,&c);
+//	s = (a+b)*c/2;
+//	printf("梯形的面积=%d\n",s);
+	
+ /*int r ;
+	scanf("%d",&r);
+	float c = 2 * PI * r;
+	printf("%f\n",c);
+	return 0;
+
+	printf("the string is: % s\n", "hello");
+	printf("the string is: % 10s\n", "hello");
+	printf("the string is: % -10s\n", "hello");
+	char cstring[30];
+	gets(cstring);
+	puts(cstring);*/
+	//int a,b,c = 10;
+	//printf("%d\n",c);
+	int a = 0.7;
+	printf("%d\n",a);
+	
+	return 0;
+}
